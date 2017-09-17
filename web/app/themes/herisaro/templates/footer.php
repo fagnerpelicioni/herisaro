1affe04b6f7e27b6e2fbd81b2e9bad286161117e
pelicioni <fagner.txt@gmail.com>
2017-09-16 22:53:33 -0300

    Alterado estilo de footer

--- web/app/themes/herisaro/templates/footer.php
+++ web/app/themes/herisaro/templates/footer.php
@@ -1,10 +1,49 @@
-<footer class="content-info">
+
+<footer>
+    
+        <div class="container">
+        <div class="row">
+        	<div class="col-md-3">
+        		<h1 class="footer-logo">Grafreez</h1>
+        	</div>
+        	<div class="col-md-2 text-center">
+        			<i class="fa fa-map"></i><br>
+                	<div>234 Wall Street<br>East Newyork, NY 20001</div>
+        	</div>
+        	<div class="col-md-2 text-center">
+        		<i class="fa fa-mobile" aria-hidden="true"></i><br>
+                <div>1800 1234 5678<br>(080) 1234 56789</div>
+        	</div>
+        	<div class="col-md-2 text-center">
+        		<i class="fa fa-envelope"></i><br>
+                	<div>
+                      	<a href="#">sales@yourcompany.com</a>
+                      	<a href="#">www.yourcompany.com</a>
+                    </div>
+        	</div>
+        	<div class="col-md-3">
+        		<div class="footer-social">
+	                    <a href="#"><i class="fa fa-linkedin"></i></a>
+	                    <a href="#"><i class="fa fa-facebook"></i></a>
+	                    <a href="#"><i class="fa fa-twitter"></i></a>
+	                    <a href="#"><i class="fa fa-dribbble"></i></a>
+	                    <a href="#"><i class="fa fa-google-plus"></i></a>
+	                    <a href="#"><i class="fa fa-instagram"></i></a>
+                    </div>
+        	</div>
+        </div>
+        </div>
+</footer>
+
+<!-- Meus dados -->
+
+<div class="content-info meus-dados">
   
         <div class="container">
             <div class="row">
-                <div class="col-sm-12 col-md-3">&copy;<script type="text/javascript">document.write(new Date().getFullYear());</script> pelicioni.com</div>
+                <div class="col-sm-12 col-md-3">&copy;<script type="text/javascript">document.write(new Date().getFullYear());</script> <a href="http://pelicioni.com">pelicioni web studios</a></div>
                 <div class="col-sm-12 col-md-2 pull-right love-text">Criado com <i class="fa fa-heart"></i> e <i class="fa fa-coffee" aria-hidden="true"></i></div>
             </div>
         
         </div>
-</footer>
+</div>
