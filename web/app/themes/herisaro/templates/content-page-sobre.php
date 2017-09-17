07718ed4f0727c4befe1254a5f8568c320f33a4b
pelicioni <fagner.txt@gmail.com>
2017-09-17 12:07:56 -0300

    novo estilo de pagina about

--- web/app/themes/herisaro/templates/content-page-sobre.php
+++ web/app/themes/herisaro/templates/content-page-sobre.php
@@ -1,20 +1,15 @@
 
-<section class="about-sec" id="about">
-    
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 col-md-6 pull-right">
-                  <div class="content-block">
-                       <h3>Who We Are</h3>
-                 <p>It suddenly struck me that that tiny pea, pretty and blue, was the Earth. I put up my thumb and shut one eye, and my thumb blotted out the planet Earth.</p>
-                <ul>
-                    <li>If you could see the earth illuminated when you were in a place as dark as night, it would look to you more splendid than the moon.</li>
-                      <li>I put up my thumb and shut one eye, and my thumb blotted out the planet Earth. I didn't feel like a giant. I felt very, very small.</li>
-                      <li>As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful. </li>
-                </ul>
-                </div>
-            </div>
-        </div>
+<section class="about-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-5">
+        <h3><?php the_title( ); ?></h3>
+        <?php the_content(); ?>
+        <a href="#" class="btn btn-transparent">Explore More</a>
+      </div>
+      <div class="col-md-6 pull-right image-sec">
+        <img class="img-responsive" src="<?php the_post_thumbnail_url(); ?>" >
+      </div>
     </div>
-      
-</section>      
+  </div>
+</section> 
