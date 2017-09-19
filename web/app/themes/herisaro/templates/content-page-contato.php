e9c3c36568c0615d0dbd15053752c50dba193725
pelicioni <fagner.txt@gmail.com>
2017-09-19 01:55:46 -0300

    Pagina de contato adicionada

--- web/app/themes/herisaro/templates/content-page-contato.php
+++ web/app/themes/herisaro/templates/content-page-contato.php
@@ -1,44 +1,34 @@
-      
-<section class="content contact-sec" id="contact">
-				<div class="container form-block">
-                <div class="row">
-                <div class="col-lg-12 text-center"><h2>Contact Us</h2></div>
-				<div class="col-lg-4 col-md-4 col-sm-12">
-                <span class="input input-hoshi">
-					<input class="input_field input_field-hoshi" type="text" id="input-4" />
-					<label class="input_label input_label-hoshi input_label-hoshi-color-1" for="input-4">
-						<span class="input_label-content input_label-content-hoshi">Name</span>
-					</label>
-				</span>
-                </div>
-				<div class="col-lg-4 col-md-4 col-sm-12">
-                <span class="input input-hoshi">
-					<input class="input_field input_field-hoshi" type="text" id="input-4" />
-					<label class="input_label input_label-hoshi input_label-hoshi-color-1" for="input-4">
-						<span class="input_label-content input_label-content-hoshi">Phone</span>
-					</label>
-				</span>
-                </div>
-				<div class="col-lg-4 col-md-4 col-sm-12">
-                <span class="input input-hoshi">
-					<input class="input_field input_field-hoshi" type="text" id="input-4" />
-					<label class="input_label input_label-hoshi input_label-hoshi-color-1" for="input-4">
-						<span class="input_label-content input_label-content-hoshi">Email</span>
-					</label>
-				</span>
-                </div>
-                
-                <div class="col-lg-12 col-md-12 col-sm-12 text-center">
-                <span class="t-area input-hoshi">
-                <textarea class="input_field input_field-hoshi" rows="1"></textarea>
-						<label class="input_label input_label-hoshi input_label-hoshi-color-1" for="input-4">
-						<span class="input_label-content input_label-content-hoshi">Email</span>
-					</label>
-				</span>
-                <a href="#" class="btn btn-tranparent">Submit</a>
-                </div>
-                
-                </div>
-                </div>
-				
-			</section>   
+<section class="contato">
+<div class="container">
+  <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">                        
+        <h3>CONTATO</h3>
+      </div>
+  </div>
+  <div class="row">
+      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xs-offset-3">
+          <form id="contact-form" class="form" action="#" method="POST" role="form">
+              <div class="form-group">
+                  <label class="form-label" for="name">Your Name</label>
+                  <input type="text" class="form-control" id="name" name="name" placeholder="Your name" tabindex="1" required>
+              </div>                            
+              <div class="form-group">
+                  <label class="form-label" for="email">Your Email</label>
+                  <input type="email" class="form-control" id="email" name="email" placeholder="Your Email" tabindex="2" required>
+              </div>                            
+              <div class="form-group">
+                  <label class="form-label" for="subject">Subject</label>
+                  <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" tabindex="3">
+              </div>                            
+              <div class="form-group">
+                  <label class="form-label" for="message">Message</label>
+                  <textarea rows="5" cols="50" name="message" class="form-control" id="message" placeholder="Message..." tabindex="4" required></textarea>                                 
+              </div>
+              <div class="text-center">
+                  <button type="submit" class="btn btn-start-order">Send Message</button>
+              </div>
+          </form>
+      </div>
+  </div>
+</div>
+</section>
